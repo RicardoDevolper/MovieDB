665fab055967f12d95e9b4040ae7556d68d1b6fc
Ricardo <ricardoDeveloper2020@gmai.com>
2020-05-03 20:27:22 -0300

    Projeto de consumo de API The Movie DB

---
+++ src/Movies/Movies/ViewModels/MenuViewModel.cs
@@ -0,0 +1,98 @@
+﻿using Movies.Models;
+using Movies.Services.Navigation;
+using Movies.ViewModels.Base;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Movies.ViewModels
+{
+    public class MenuViewModel : ViewModelBase
+    {
+        private ObservableCollection<MenuItem> _menuItems;
+
+        private INavigationService _navigationService;
+
+        public MenuViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public ObservableCollection<MenuItem> MenuItems
+        {
+            get
+            {
+                return _menuItems;
+            }
+            set
+            {
+                _menuItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ItemSelectedCommand => new Xamarin.Forms.Command<MenuItem>(SelectMenuItem);
+
+        public override Task InitializeAsync(object navigationData)
+        {
+            InitMenuItems();
+
+            return base.InitializeAsync(navigationData);
+        }
+
+        private void InitMenuItems()
+        {
+            MenuItems = new ObservableCollection<MenuItem>
+            {
+                new MenuItem
+                {
+                    Title = "Navegar",
+                    MenuItemType = MenuItemType.Discover,
+                    ViewModelType = typeof(HomeViewModel),
+                    IsEnabled = true
+                },
+                new MenuItem
+                {
+                    Title = "Filmes",
+                    MenuItemType = MenuItemType.Movies,
+                    ViewModelType = typeof(MoviesViewModel),
+                    IsEnabled = true
+                },
+                new MenuItem
+                {
+                    Title = "TV e Shows",
+                    MenuItemType = MenuItemType.TVShows,
+                    ViewModelType = typeof(ShowsViewModel),
+                    IsEnabled = true
+                },
+                new MenuItem
+                {
+                    Title = "Lançamentos",
+                    MenuItemType = MenuItemType.Upcoming,
+                    ViewModelType = typeof(UpcomingViewModel),
+                    IsEnabled = true
+                },
+                new MenuItem
+                {
+                    Title = "Favoritos",
+                    MenuItemType = MenuItemType.Favourites,
+                    IsEnabled = false
+                },
+                new MenuItem
+                {
+                    Title = "Configurações",
+                    MenuItemType = MenuItemType.Settings,
+                    IsEnabled = false
+                }
+            };
+        }
+
+        private async void SelectMenuItem(MenuItem item)
+        {
+            if (item.IsEnabled)
+            {
+                await _navigationService.NavigateToAsync(item.ViewModelType, null);
+            }
+        }
+    }
+}
